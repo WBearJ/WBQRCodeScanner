d3c74a2f3688068fd9d8629a033297687ba8c809
noob <karuasha@gmail.com>
2024-03-27 23:33:59 +0800

    update demo

--- WBQRCodeScannerDemo/WBQRCodeScannerDemo/ContentView.swift
+++ WBQRCodeScannerDemo/WBQRCodeScannerDemo/ContentView.swift
@@ -12,28 +12,78 @@ struct ContentView: View {
     
     @State var results = [WBQRBarcodeResult]()
     
-    @State var scannerActive = false
+    @State var defaultLinkActive = false
+    
+    @State var customizeLinkActive = false
     
     var body: some View {
         NavigationView {
-            VStack(spacing: 30) {
+            VStack {
+                // defalut preview
                 NavigationLink(
-                    destination: WBQRScanner {
-                                    WBQRScanPreview(results: $results)
-                                } scanResults: { results in
-                                    self.results = results
-                                    scannerActive.toggle()
-                                },
-                    isActive: $scannerActive,
+                    destination: defaultPreview(),
+                    isActive: $defaultLinkActive,
                     label: {
-                        Text("Scan")
+                        Text("Default Scan Preview")
                     })
                 
-                ForEach(results, id: \.stringValue) { result in
-                    Text("Scan Value: \(result.stringValue ?? "")")
+                // customize preview
+                NavigationLink(
+                    destination: customizePreview(),
+                    isActive: $customizeLinkActive,
+                    label: {
+                        Text("Customized Scan Preview")
+                    })
+                .padding(.top, 20)
+                
+                // scan value
+                if !results.isEmpty {
+                    // title
+                    Text("Scan Result")
+                        .font(.title3)
+                        .padding(.top, 10)
+                    // results
+                    ForEach(results, id: \.stringValue) { result in
+                        Text("Result: \(result.stringValue ?? "")")
+                    }
                 }
             }
             .padding()
+        }
+    }
+}
+
+// MARK: - Default Preview
+extension ContentView {
+    func defaultPreview() -> some View {
+        WBQRScanner {
+            WBQRScanPreview(results: $results)
+        } scanResults: { results in
+            self.results = results
+            defaultLinkActive.toggle()
+        }
+    }
+}
+
+// MARK: - Customize Preview
+extension ContentView {
+    func customizePreview() -> some View {
+        WBQRScanner {
+            VStack(spacing: 20) {
+                // text
+                Text("This is customize preview")
+                    .foregroundColor(.white)
+                
+                // dismiss
+                Button {
+                    customizeLinkActive.toggle()
+                } label: {
+                    Text("Dismiss")
+                }
+            }
+        } scanResults: { results in
+            self.results = results
+            customizeLinkActive.toggle()
         }
     }
 }
